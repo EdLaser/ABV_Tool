ded323d9325c72a69caef979206f4b96a80e2424
Ruben-David <ruben-david.kraus@ottogroup.com>
2023-06-30 13:31:35 +0200

    removed mesaurment

--- manage.py
+++ manage.py
@@ -4,44 +4,19 @@ import os
 import sys
 
 
-from codecarbon import EmissionsTracker
 
-with EmissionsTracker() as tracker:
-    def main():
-        import logging
-
-        logger = logging.getLogger("codecarbon")
-        while logger.hasHandlers():
-            logger.removeHandler(logger.handlers[0])
-
-        # Define a log formatter
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)-12s: %(levelname)-8s %(message)s"
-        )
-
-        # Create file handler which logs debug messages
-        fh = logging.FileHandler("codecarbon.log")
-        fh.setLevel(logging.DEBUG)
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
-
-        consoleHandler = logging.StreamHandler(sys.stdout)
-        consoleHandler.setFormatter(formatter)
-        consoleHandler.setLevel(logging.WARNING)
-        logger.addHandler(consoleHandler)
-
-        logger.debug("GO!")
-        """Run administrative tasks."""
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_AT.settings')
-        try:
-            from django.core.management import execute_from_command_line
-        except ImportError as exc:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            ) from exc
-        execute_from_command_line(sys.argv)
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_AT.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
